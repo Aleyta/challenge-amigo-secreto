a71927dd4bca2b2109115fcd11eaec00dcbaa690
Paola Alejandra Barajas Savin <37116596+Aleyta@users.noreply.github.com>
2025-08-18 08:19:41 -0700

    Se limpio codigo js

--- app.js
+++ app.js
@@ -3,28 +3,30 @@
 
 let ListaDeNombres = [];
 
-//AGREGAR AMIGOS
+//AGREGAR AMIGOS:
+
 function añadir()
 {
-    let inputAmigo = document.getElementById('amigo'); //selecciona del documento html la informacion y se guarda en la variable inputAmigo
-    let nombreAmigo = inputAmigo.value; //agregamos el valor que se agrege a la caja de input a la variable nombreAmigo
+    let inputAmigo = document.getElementById('amigo'); 
+    let nombreAmigo = inputAmigo.value; 
     
-    if (nombreAmigo === "") {  // Valida campo vacio
+    if (nombreAmigo === "") {  
         alert('Campo vacio, inserta un nombre válido');
     } else{
-        if (ListaDeNombres.includes(nombreAmigo)) {  // si el nombre esta incluido en la lista
+        if (ListaDeNombres.includes(nombreAmigo)) {  
             alert('El nombre ya esta incluido en la lista, favor de agregar uno diferente')
         } else {
-            ListaDeNombres.push(nombreAmigo);//añade a la lista el nombre del amigos
+            ListaDeNombres.push(nombreAmigo);
             mostrarListaAmigos(); 
-            inputAmigo.value = '';  //limpia campo de texto
+            inputAmigo.value = ''; 
         }
     }
 }
 
 
 
-//MOSTRAR LISTA
+//MOSTRAR LISTA:
+
 function mostrarListaAmigos() 
 {
     let lista = document.getElementById('listaAmigos');
@@ -33,12 +35,14 @@ function mostrarListaAmigos()
     for (let i = 0; i < ListaDeNombres.length; i++) {
         let item = document.createElement('li'); 
         item.textContent = ListaDeNombres[i]; 
-        lista.appendChild(item); //inserta la informacion agregada en la lista en item (li)
+        lista.appendChild(item); 
     }
 }
 
 
-//SORTEAR NOMBRES
+
+//SORTEAR NOMBRES:
+
 function sortearAmigo() 
 {
     let amigoAleatorio = Math.floor(Math.random() * ListaDeNombres.length);
@@ -51,42 +55,34 @@ function sortearAmigo()
     {
        mostrarAmigoSecreto(amigoSecreto);
        limpiar();
-       deshabilitar();
+       document.querySelector("#sortear").setAttribute("disabled", "true");
+    //    deshabilitar();
     } 
 }
 
-function deshabilitar(){
- document.querySelector("#sortear").setAttribute("disabled", "true"); //deshabilitar el boton de nuevo juego
-//  
-}
 
-//MOSTRAR AMIGO SECRETO
+//MOSTRAR AMIGO SECRETO:
 function mostrarAmigoSecreto(amigoSecreto) 
 {
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `El amigo secreto es: <strong>${amigoSecreto}</strong>`;
-     document.querySelector("#agregar").setAttribute("disabled", "true"); //deshabilitar el boton de nuevo juego
-    
+    document.querySelector("#agregar").setAttribute("disabled", "true"); //deshabilitar el boton de nuevo juego
 }
 
 
+//LIMPIA LISTA DE AMIGOS:
 function limpiar() {
     document.getElementById("listaAmigos").innerHTML = "";
 }
 
 
+//REINICIA EL SORTEO:
 function reiniciar() {
     ListaDeNombres = [];
     mostrarListaAmigos();
-    limpiarResultado();
+    document.getElementById("resultado").innerHTML = "";
     document.getElementById("sortear").removeAttribute("disabled");
     document.getElementById("agregar").removeAttribute("disabled");
-
-
-}
-
-function limpiarResultado() {
-    document.getElementById("resultado").innerHTML = "";
 }
 
 sortearAmigo();
